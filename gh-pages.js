835e7ff89732450a626f032735172a4d9f232b37
Tuyet-Van Huynh <van@Tuyet-Vans-MacBook-Pro.local>
2022-04-16 11:57:23 -0400

    add gh pages

--- gh-pages.js
+++ gh-pages.js
@@ -1,6 +1,10 @@
-var ghpages = require('gh-pages');
+var ghpages = require("gh-pages");
 
-ghpages.publish('build', {
-  branch: 'master',
-  repo: 'https://github.com/worksbyvan/worksbyvan.github.io.git'
-}, () => console.log("Completed push to master."));
+ghpages.publish(
+  "build",
+  {
+    branch: "master",
+    repo: "https://github.com/worksbyvan/worksbyvan.github.io.git",
+  },
+  (res) => console.log("Completed push to master.", res)
+);
